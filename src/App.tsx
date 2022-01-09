105f18d02ff5b22653205d3a3c5432c779afca81
Victor Hugo <valvesperricci@yahoo.com.br>
2022-01-09 03:01:25 -0300

    iniciando servidor de mirage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { makeServer } from "./services/mirage";
+
 
 // Components
 import { Header } from "./components/Header";
 
 // Pages
 import { Home } from "./pages/Home";
+
+if (process.env.NODE_ENV === 'development') makeServer()
 
 function App() {
   return (
