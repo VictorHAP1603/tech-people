fd37f72a01c1a356e495fa2eb33e2f70ea130acd
VictorHugo <victor.hugo@newpost.com>
2022-01-10 16:51:11 -0300

    configurando base url no env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeServer } from "./services/mirage";
 
-
 // Components
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -9,7 +8,8 @@ import { Footer } from "./components/Footer";
 // Pages
 import { Home } from "./pages/Home";
 
-if (process.env.NODE_ENV === 'development') makeServer()
+// if (process.env.NODE_ENV === 'development') makeServer()
+makeServer();
 
 function App() {
   return (
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,18 @@
-import axios from 'axios'
+import axios from "axios";
 import { GetNewsProps } from "../pages/Home/News";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
-})
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
 
 export const GET_NEWS = async (): Promise<GetNewsProps[]> => {
-  const { data } = await api.get('/news')
+  const { data } = await api.get("/news");
 
   return data;
 };
 
-
 export const GET_SCHEDULES = async () => {
-  const { data } = await api.get('/schedules')
+  const { data } = await api.get("/schedules");
 
-  return data
-}
+  return data;
+};
