81cf206f60518c009a204e4941aa4ca2ab0bc1c7
Victor Hugo <valvesperricci@yahoo.com.br>
2022-01-11 17:11:58 -0300

    retirando variaveis nao usadas

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useMedia = (media: string) => {
     const [match, setMatch] = useState(false)
